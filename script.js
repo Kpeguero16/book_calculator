a084a1e89deb38d5b0c15173dacae0cd1b76974f
Kpeguero16 <khalilpeguero@hotmail.com>
2023-04-02 09:43:14 -0400

    Update

---
+++ script.js
@@ -0,0 +1,17 @@
+function getResult()
+{
+    var bookName = document.getElementById("bookName").value;
+    var pages = document.getElementById("pages").value;
+    var time = document.getElementById("time").value;
+
+    var book = bookName;
+    var nPages = pages;
+    var timeInDays = time;
+
+    console.log(book);
+    console.log(nPages);
+    console.log(timeInDays);
+}
+
+
+
